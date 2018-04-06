676b9cb67855d0e5dc95584ac759847d34ad7229
Angie Siu <asiu@mscgroupny.com>
2018-04-06 12:36:03 -0400

    update readme

--- src/_components/Header.js
+++ src/_components/Header.js
@@ -5,7 +5,6 @@ export default class Header extends React.Component {
 		return (
 			<header>
 		        <p><i className="fa fa-exclamation"></i>&nbsp;<em>This is a front-end-only project.&nbsp;<i className="fa fa-exclamation"></i></em></p>
-		        <p style={{color:'#ff0000', fontWeight: 'bold'}}><i className="fa fa-exclamation"></i>&nbsp;<em>Work In Progress version!&nbsp;<i className="fa fa-exclamation"></i></em></p>
 		    </header>
 		);
 	}
